06bf8924315d941ba512077f006eff5f2f8475b8
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-09-17 13:59:09 -0400

    progress

--- README.md
+++ README.md
@@ -20,3 +20,6 @@ screenshot of app in readme
 
 submit live page and repo
 ```
+
+public/ ===> manifest.json, service-worker.js
+
--- public/js/idb.js
+++ public/js/idb.js
@@ -1,1 +1,62 @@
-//for indexed db
+let db;
+const request = indexedDB.open("budget_tracker", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("new_transaction", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    uploadBudget();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+  budgetObjectStore.add(record);
+}
+
+function uploadBudget() {
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+  const getAll = budgetObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          const budgetObjectStore = transaction.objectStore("new_transaction");
+          budgetObjectStore.clear();
+
+          alert("All saved transactions have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener('online', uploadBudget);
